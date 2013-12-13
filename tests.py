e216c15ad4367b2da3d05f2e7cfb3d255798c6b9
Glyn Mooney <glyn.mooney@gmail.com>
2013-12-13 13:44:16 +0000

    Initial commit
    Created new git repo

---
+++ tests.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+class TestCasts(TestCase):
+    
+    def can_get_file(self):
+        pass
+    
+    def can_delete_file(self):
+        pass
+    
+    def can_get_list_of_outdated_files(self):
+        pass
+    
+    def can_delete_files(self):
+        pass
